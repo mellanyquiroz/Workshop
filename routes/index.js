51d402af3c3ebf049a658ec2458bb14744d001d6
Mellany Quiroz <quirozmellany@gmail.com>
2018-09-15 16:50:20 -0700

    More recent updates...

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/new page', function(req, res, next) {
+  res.render('new page', { title: 'New page!'});
+});
+
 module.exports = router;
