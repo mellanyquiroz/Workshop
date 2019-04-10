7b4732d62df7b32127aa43290743bb2198fd46c5
Mellany Quiroz <quirozmellany@gmail.com>
2019-04-10 02:56:39 -0700

    Mission Page Updated

--- routes/mission.js
+++ routes/mission.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET mission page. */
-router.get('/mission', function(req, res, next) {
-  res.render('mission', {
-    title: 'Mission'
+router.get('/', function(req, res, next) {
+  res.render('mission/show', {
+     title: 'Mission!'
    });
-});
-
-router.get('/new-page', function(req, res, next) {
-  res.render('new-page', { title: 'New page!'});
 });
 
 module.exports = router;
